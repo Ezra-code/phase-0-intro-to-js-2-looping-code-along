216773773a6d97062f01be77f757eb818b69c6ac
Ezra-code <njerukithinji872@gmail.com>
2022-06-01 14:40:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(thankYou, event){
+    let thanks = []
+    for(let i = 0; i < thankYou.length; i++){
+         let message =`Thank you, ${thankYou[i]}, for the wonderful ${event} gift!`
+         thanks.push(message)
+    }
+    return thanks
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "graduation"))
+function countDown(count){
+    while(count >= 0){
+        console.log(count--)
+    }
+}
