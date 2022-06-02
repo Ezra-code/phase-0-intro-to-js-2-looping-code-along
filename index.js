df3696f8e38ad2b2809990ff9503e252fcf8f8fc
Ezra-code <njerukithinji872@gmail.com>
2022-06-02 16:24:44 +0300

    commit tree 66a5132b31f29475610554c21a84e498998b9e11

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-// Code your solutions in this file
-function writeCards(thankYou, event){
-    let thanks = []
-    for(let i = 0; i < thankYou.length; i++){
-         let message =`Thank you, ${thankYou[i]}, for the wonderful ${event} gift!`
-         thanks.push(message)
-    }
-    return thanks
+// // Code your solutions in this file
+// function writeCards(thankYou, event){
+//     let thanks = []
+//     for(let i = 0; i < thankYou.length; i++){
+//          let message =`Thank you, ${thankYou[i]}, for the wonderful ${event} gift!`
+//          thanks.push(message)
+//     }
+//     return thanks
+// }
+// function countDown(count){
+//     while(count >= 0){
+//         console.log(count--)
+//     }
+// }
+
+// let names = ["Guadalupe", "Ollie", "Aki"]
+// for (const name of names) {
+//     console.log(name)
+// }
+let count = 10;
+
+function countDown (count){
+while (count > 0) {
+    console.log(count--);
 }
-function countDown(count){
-    while(count >= 0){
-        console.log(count--)
-    }
+
 }
+
+console.log(countDown());
